c8ef48d965a7a9d1a8497a8e48328d824ac903da
Anil Hashia <anhashia@microsoft.com>
2016-07-13 12:54:51 -0700

    Return type of IsSharedAccessSignature should be BOOL for all uses cases

--- csharp/device/Microsoft.Azure.Devices.Client/Common/Security/SharedAccessSignature.cs
+++ csharp/device/Microsoft.Azure.Devices.Client/Common/Security/SharedAccessSignature.cs
@@ -125,11 +125,22 @@ namespace Microsoft.Azure.Devices.Client
                 return false;
             }
 
-            IDictionary<string, string> parsedFields = ExtractFieldValues(rawSignature);
-            string signature;
-            bool isSharedAccessSignature = parsedFields.TryGetValue(SharedAccessSignatureConstants.SignatureFieldName, out signature);
 
-            return isSharedAccessSignature;
+            try
+            {
+                IDictionary<string, string> parsedFields = ExtractFieldValues(rawSignature);
+                string signature;
+                bool isSharedAccessSignature = parsedFields.TryGetValue(SharedAccessSignatureConstants.SignatureFieldName, out signature);
+                return isSharedAccessSignature;
+            }
+
+            catch (FormatException)
+            {
+                return false;
+            }
+
+
+
         }
 
         public bool IsExpired()
